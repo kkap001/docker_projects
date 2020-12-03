8bc0a0eac102703f07d5f93ef91cb33000d183f1
kkap001 <57065906+kkap001@users.noreply.github.com>
2020-12-03 11:43:18 +0530

    Update gunicorn_process.sh

--- fangular_bdjango/django/gunicorn_process.sh
+++ fangular_bdjango/django/gunicorn_process.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cd /var/www/html/LendingAlly/
+cd /var/www/html/backendpath/
 source venv/bin/activate  #assuming you use virtualenv
-cd lendingbackend
+
 #workon your_env_name # if you do not use virtualenv, remove this line the line above
 gunicorn --bind 0.0.0.0:7000 --workers=9 --threads=9 LendingAlly.wsgi:application 
